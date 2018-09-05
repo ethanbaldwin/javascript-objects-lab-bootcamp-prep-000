9750b96077760c8716d1ada35aa23c3add88fd1e
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-09-05 00:56:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object);
   delete newObject.key;
+  return newObject;
 }
 
 function  destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
+  return object;
 }
